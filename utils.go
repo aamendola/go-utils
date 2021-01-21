0486dc5c9d3be99e613d860e78db08e6fe8997eb
alejandro <alejandro.amendola@gmail.com>
2021-01-21 12:27:20 -0300

    Automatic commit message

--- utils.go
+++ utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"log"
 )
 
@@ -18,4 +19,23 @@ func PanicOnError(err error) {
 
 func PrintTypeAndValue(description string, obj interface{}) {
 	log.Printf("==[%s, %T, %s]\n", description, obj, obj)
+}
+
+// Color ...
+type Color string
+
+const (
+	colorReset  Color = "\033[0m"
+	colorRed    Color = "\033[31m"
+	colorGreen  Color = "\033[32m"
+	colorYellow Color = "\033[33m"
+	colorBlue   Color = "\033[34m"
+	colorPurple Color = "\033[35m"
+	colorCyan   Color = "\033[36m"
+	colorWhite  Color = "\033[37m"
+)
+
+// PrintColor ...
+func PrintColor(color Color, str string) {
+	fmt.Printf(string(color) + str + string(colorReset))
 }
